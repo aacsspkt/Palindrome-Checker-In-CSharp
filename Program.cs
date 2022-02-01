8bebcea5adca400db6b61de3627d2d7b80cf2823
Ashish Sapkota <ashishspkt6566@gmail.com>
2022-02-01 23:06:54 +0545

    feat: add palindrome checker
    body: add method that ask for string input and check if it is palindrome of not.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// running program
+run();
+
+
+void run()
+{
+    Console.Clear();
+    // ask for input
+    Console.Write("Enter a value to check palindrome: ");
+
+    // capture input
+    string? input = Console.ReadLine();
+    if (!string.IsNullOrEmpty(input))
+    {
+        string output = isPalindrome(input) ? "It is a palindrome" : "It is not palindrome";
+        Console.WriteLine(output);
+    }
+    else
+    {
+        run();
+    }
+
+
+
+}
+
+bool isPalindrome(string input)
+{
+    // len is length of input value 
+    int len = input.Length;
+
+    // looping up to half of length of string
+    for (int i = 0; i < len / 2; i++)
+    {
+        // if first not equals last it is not palindrome;
+        if (input[i] != input[len - i - 1]) return false;
+    }
+
+    // if method does not return false till now then it is palindrome.
+    return true;
+}
+
+
